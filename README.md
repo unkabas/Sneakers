7efdc736cefd558aea4fa27c6a7ea2cf722eeaa0
unkabas <undercannabis10@gmail.com>
2025-05-01 15:07:51 +0200

    first commit

--- README.md
+++ README.md
@@ -31,3 +31,4 @@ npm run dev
 ```sh
 npm run build
 ```
+# vue_sneak
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+
 	plugins: [vue(), vueDevTools(), tailwindcss()],
 	resolve: {
 		alias: {
